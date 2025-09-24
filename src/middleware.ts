4fddc82e0fb28a7de439412e38f368605f536f3e
debasish-mishra-fncr <debasish.mishra@fncr.com>
2025-09-24 20:42:17 +0530

    refactor middleware to allow registered users to signin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,18 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-const publicRoutes = ["/"];
-
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
-
+  const publicRoutes = ["/"];
+  // Redirect authenticated users away from root ("/") to /properties
   if (token && pathname === "/") {
-    return NextResponse.redirect(new URL("/onboardingA", req.url));
+    return NextResponse.redirect(new URL("/onboarding", req.url));
   }
-
-  if (!token && !publicRoutes.includes(pathname)) {
+  // Redirect unauthenticated users trying to access protected routes
+  if (!token && !publicRoutes.some((route) => pathname.startsWith(route))) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|ico|webp)).*)",
-  ],
-};
